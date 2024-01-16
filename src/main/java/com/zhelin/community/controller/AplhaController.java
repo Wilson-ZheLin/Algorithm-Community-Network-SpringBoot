cb246ac31dde3d84c463ff39ba5b5d94489064ee
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2024-01-16 01:10:28 -0500

    Feature Add: Verification Code Generation

--- src/main/java/com/zhelin/community/controller/AplhaController.java
+++ src/main/java/com/zhelin/community/controller/AplhaController.java
@@ -1,15 +1,55 @@
 package com.zhelin.community.controller;
 
+import com.zhelin.community.util.CommunityUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/alpha")
 public class AplhaController {
+
     @RequestMapping("/hello")
     @ResponseBody
     public String sayHello() {
         return "Hello Spring Boot!";
+    }
+
+    // Cookie
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        cookie.setPath("/community");
+        cookie.setMaxAge(60 * 60 * 1);
+        response.addCookie(cookie);
+
+        return "set cookie";
+    }
+
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code) {
+        return "get cookie";
+    }
+
+    // Session
+    @RequestMapping(path = "/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("code", CommunityUtil.generateUUID());
+        return "set session";
+    }
+
+    @RequestMapping(path = "/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        return session.getAttribute("code").toString();
     }
 }
--- src/main/java/com/zhelin/community/controller/LoginController.java
+++ src/main/java/com/zhelin/community/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.zhelin.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.zhelin.community.entity.User;
 import com.zhelin.community.service.UserService;
 import com.zhelin.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -60,6 +74,21 @@ public class LoginController implements CommunityConstant {
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        session.setAttribute("kaptcha", text);
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("Response kaptcha error:" + e.getMessage());
+        }
     }
 
 }

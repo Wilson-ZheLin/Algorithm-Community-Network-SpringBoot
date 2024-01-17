7034ec0f7b749acc60e521ff510951b91e5ff846
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2024-01-17 05:30:23 -0500

    Feature Add: Login required checking

--- src/main/java/com/zhelin/community/config/WebMvcConfig.java
+++ src/main/java/com/zhelin/community/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.zhelin.community.config;
 
+import com.zhelin.community.controller.Interceptor.LoginRequiredInterceptor;
 import com.zhelin.community.controller.Interceptor.LoginTicketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +13,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
 
+    @Autowired
+    private LoginRequiredInterceptor loginRequiredInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginTicketInterceptor).excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+        registry.addInterceptor(loginRequiredInterceptor).excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
--- src/main/java/com/zhelin/community/controller/UserController.java
+++ src/main/java/com/zhelin/community/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.zhelin.community.controller;
 
+import com.zhelin.community.annotation.LoginRequired;
 import com.zhelin.community.entity.User;
 import com.zhelin.community.service.UserService;
 import com.zhelin.community.util.CommunityUtil;
@@ -43,11 +44,13 @@ public class UserController {
     @Autowired
     private HostHolder hostHolder;
 
+    @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
     public String getSettingPage() {
         return "/site/setting";
     }
 
+    @LoginRequired
     @RequestMapping(path = "/upload", method = RequestMethod.POST)
     public String uploadHeader(MultipartFile headerImage, Model model) {
         if(headerImage == null) {
@@ -97,6 +100,7 @@ public class UserController {
         }
     }
 
+    @LoginRequired
     @RequestMapping(path = "/changePassword", method = RequestMethod.POST)
     public String changePassword(String oldPassword, String newPassword, Model model) {
         if(StringUtils.isBlank(newPassword)) {
